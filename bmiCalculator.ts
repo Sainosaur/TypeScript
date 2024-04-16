9ff81de39a561b6e9b6ca0dbcdd11401c98fce03
adirajus <saiadi4002@gmail.com>
2024-04-16 19:22:02 +0100

    Exercise 9.1 Implemented calculateBmi function in typeScript

---
+++ bmiCalculator.ts
@@ -0,0 +1,13 @@
+
+const calculateBmi = (height: number, weight: number) => {
+    const BMI: number = weight / Math.pow((height / 100), 2);
+    if (BMI < 18.5) {
+        return "Under-weight"
+    } else if ( 18.5 < BMI && 24.9 > BMI ) {
+        return "Normal (healthy weight)"
+    } else if ( 24.9 < BMI) {
+        return "Over-weight"
+    }
+}
+
+console.log(calculateBmi(180, 74))
