70f26b11c464a5af2d9210400fb1bd8d56fc62dc
adirajus <saiadi4002@gmail.com>
2024-04-16 19:41:46 +0100

    Exercise 9.2 Implemented exercise calculator function

---
+++ exerciseCalculator.ts
@@ -0,0 +1,40 @@
+interface Result {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number
+}
+// Remember going forward, output type always comes after parameters and before the arrow notation.
+
+const exerciseCalculator = (hours: number[]): Result => {
+    const trainingDays = hours.filter(day => day !== 0);
+    let totalHours = 0;
+    let rating;
+    let ratingDescription;
+    trainingDays.forEach(hours => totalHours += hours);
+    const average = totalHours / hours.length;
+    if (average > 3) {
+        rating = 3
+        ratingDescription = "Excellent Job !"
+    } if (average > 1) {
+        rating = 2
+        ratingDescription = "Good Job but can do better !"
+    } if (average < 1) {
+        rating = 1
+        ratingDescription = "Significant to improve..."
+    }
+    return {
+        periodLength: hours.length,
+        trainingDays: trainingDays.length,
+        success: average >= 2,
+        rating,
+        ratingDescription,
+        target: 2,
+        average
+    }
+}
+
+console.log(exerciseCalculator([3, 0, 2, 4.5, 0, 3, 1]))
