8d7ddc89dd3c8dee552ba69825dd0fe557768e75
adirajus <saiadi4002@gmail.com>
2024-05-01 12:10:08 +0100

    Exercise 9.22 Typed entries using union type for different kinds of diagnoses

--- Patientor/src/types.ts
+++ Patientor/src/types.ts
@@ -1,6 +1,46 @@
-
-export interface Entry {
+export enum HealthCheckRating {
+"Healthy" = 0,
+"LowRisk" = 1,
+"HighRisk" = 2,
+"CriticalRisk" = 3
 }
+
+export interface discharge {
+date: string;
+criteria: string;
+}
+
+export interface sickLeave {
+startDate: string;
+endDate: string;
+}
+
+export interface BasicEntry {
+id: string;
+date: string;
+specialist: string;
+diagnosisCodes?: string[];
+description: string
+}
+
+export interface HopsitalEntry extends BasicEntry {
+type: "Hospital";
+discharge: discharge;
+}
+
+export interface OccupationalHealthcareEntry extends BasicEntry {
+type: "OccupationalHealthcare";
+employerName: string;
+sickLeave?: sickLeave;
+}
+
+export interface HealthCheckEntry extends BasicEntry {
+type: "HealthCheck";
+healthCheckRating: HealthCheckRating;
+}
+
+export type Entry = HopsitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
+
 
 export enum Gender {
     Male =  "male",
